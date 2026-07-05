52cecf96986498670c81b43fcf2d35949fd12d2b
abinaya <abinayajayaraman30@gmail.com>
2026-07-06 01:42:38 +0530

    first commit

---
+++ Day-2/createPhase/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./component/Header";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome To React JS</h1>
+
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day-2/createPhase/src/component/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h2>My Website Header</h2>
+    </header>
+  );
+}
+
+export default Header;
